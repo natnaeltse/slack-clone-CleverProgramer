757799bc4a302cf163b5255b04b8a8f1da318ef2
Natnael Tsegaw <natnaeltsegaw83@gmail.com>
2021-04-04 04:05:07 +0200

    Firebase database connect to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,32 @@
 import './CSS/Sidebar.css'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SidebarOption from './SidebarOption'
+import db from '../firebase'
 
 function Sidebar() {
+	const [channels, setChannels] = useState([])
+
+	useEffect(() => {
+		db.collection('room').onSnapshot((snapshot) => {
+			setChannels(
+				snapshot.docs.map((doc) => {
+					return {
+						id: doc.id,
+						name: doc.data().name,
+					}
+				})
+			)
+		})
+	}, [])
+
+	const channelsListDisplay = () => {
+		return channels.map((channel) => {
+			return (
+				<SidebarOption title={channel.name} id={channel.id} key={channel.id} />
+			)
+		})
+	}
+
 	return (
 		<div className='sidebar'>
 			<div className='sidebar__header'>
@@ -26,13 +50,48 @@ function Sidebar() {
 			</div>
 
 			<SidebarOption
+				icon={<i className='fas fa-comment-alt'></i>}
+				title='Threads'
+			/>
+			<SidebarOption
+				icon={<i className='fas fa-quote-right'></i>}
+				title='Mention & Rection'
+			/>
+			<SidebarOption
+				icon={<i className='fas fa-folder-open'></i>}
+				title='Saved Items'
+			/>
+			<SidebarOption
+				icon={<i className='fas fa-award'></i>}
+				title='Channel Browser'
+			/>
+			<SidebarOption
+				icon={<i className='fas fa-user-friends'></i>}
+				title='People & User groups'
+			/>
+			<SidebarOption
+				icon={<i className='fab fa-buromobelexperte'></i>}
+				title='Apps'
+			/>
+			<SidebarOption
+				icon={<i className='fas fa-copy'></i>}
+				title='File Browser'
+			/>
+			<SidebarOption
+				icon={<i className='fas fa-caret-up'></i>}
+				title='Show less'
+			/>
+			<hr />
+			<SidebarOption
 				icon={<i className='fas fa-sort-down'></i>}
 				title='Channels'
 			/>
-			<SidebarOption title='YouTube' />
-			<SidebarOption title='YouTube' />
-			<SidebarOption title='YouTube' />
-			<SidebarOption title='YouTube' />
+			<hr />
+
+			{channelsListDisplay()}
+			{/* <SidebarOption title='YouTube' /> */}
+			{/* <SidebarOption title='YouTube' />
+			<SidebarOption title='YouTube' /> */}
 		</div>
 	)
 }
--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -1,17 +1,24 @@
 import './CSS/SidebarOption.css'
 import React from 'react'
 
-function SidebarOption({ icon, title }) {
+function SidebarOption({ icon, title, id }) {
 	return (
-		<div className='sidebarOption'>
+		<div className='sidebarOption' id={id}>
 			{/* if we pass icon value then it's going to render a heading */}
 			{/* when no icon is passed then we are going to render a channel */}
 			{icon ? (
-				<div className='sidebarOption__icon'>
-					{icon}
-					<p>{title}</p>
-					<i className='fas fa-plus'></i>
-				</div>
+				title === 'Channels' ? (
+					<div className='sidebarOption__icon'>
+						{icon}
+						<p>{title}</p>
+						<i className='fas fa-plus'></i>
+					</div>
+				) : (
+					<div className='sidebarOption__icon'>
+						{icon}
+						<p>{title}</p>
+					</div>
+				)
 			) : (
 				<h3 className='sidebarOption__channel'>
 					<span className='sidebarOption__hash'> # </span>
